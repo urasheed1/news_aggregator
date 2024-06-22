094b50e2f563fe662e422d72adcad8ea3f39e559
Umer <umer.rasheed@stonybrook.edu>
2024-06-22 15:50:18 -0400

    c

--- app/routes.py
+++ app/routes.py
@@ -5,28 +5,29 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    news = get_news()
-    print(news)  # Debug print statement to check the news data
+    news = get_all_news()
     return render_template('index.html', news=news)
 
 @main.route('/api/news')
 def api_news():
-    news = get_news()
+    news = get_all_news()
     return jsonify(news)
 
-def get_news():
+def get_news_by_category(category):
     url = "https://newsapi.org/v2/top-headlines"
     params = {
         'country': 'us',  # Change the country code as needed
-        'apiKey': 'f2e85a990df44c5fa9cb0110bdd6e807'  # Your actual News API key
+        'category': category,
+        'apiKey': 'f2e85a990df44c5fa9cb0110bdd6e807'  # Replace with your actual News API key
     }
     response = requests.get(url, params=params)
-    print("Response status code:", response.status_code)  # Debugging line
-    print("Response text:", response.json())  # Print the response JSON
-
     if response.status_code == 200:
         articles = response.json().get('articles', [])
-        news = [{'title': article['title'], 'link': article['url']} for article in articles]
-        return news
+        return [{'title': article['title'], 'link': article['url'], 'source': article['source']['name']} for article in articles]
     else:
         return []
+
+def get_all_news():
+    categories = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology']
+    news = {category: get_news_by_category(category) for category in categories}
+    return news
